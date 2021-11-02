d40f9f3fce0d3082fd6d4bbf96244c822a2227b5
scokljat <90629541+scokljat@users.noreply.github.com>
2021-11-02 20:11:23 +0100

    Adds articles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, { useState, useEffect } from "react";
-import Article from "./components/Article";
+import { useState, useEffect } from "react";
 import SideNav from "./components/SideNav";
-import Main from "./components/Main";
+import Article from "./components/Article";
+
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState(" ");
+  let [list, setList] = useState([]);
+
+  useEffect(
+    async function fetchData() {
+      const response = await fetch("https://www.reddit.com/r/popular.json");
+      const articles = await response.json();
+      const article = articles.data.children;
+      setList(article);
+    },
+    [subreddit]
+  );
+
   return (
     <div>
       <SideNav />
-      <Main />
+      <h1>Articles</h1>
+      {list.map((item) => {
+        console.log(item);
+        return <div>{item.data.title}</div>;
+      })}
     </div>
   );
 }
-
 export default App;
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,10 @@
-function Article(key, index) {
-  return <Article />;
+import { useState, useEffect } from "react";
+function Article(props) {
+  const [title, setTitle] = useState("");
+  {
+    props.items.map((item) => <div key={item.id} title={item.title}></div>);
+  }
+  return <div>{title}</div>;
 }
 
 export default Article;
